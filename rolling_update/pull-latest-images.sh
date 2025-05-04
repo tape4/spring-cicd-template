cc3e67a0587c630de55d7d3d922a885919a0af11
JeongHoon Lee <sherry0_@kakao.com>
2025-05-04 17:58:37 +0900

    fix storage init.sql fields to use env variables

--- rolling_update/pull-latest-images.sh
+++ rolling_update/pull-latest-images.sh
@@ -16,10 +16,3 @@ IMAGE_NAME="$DOCKER_ACCOUNT_ID/$DOCKER_REPOSITORY_NAME:${DOCKER_IMAGE_TAG}"
 # Pull the latest image from DOCKER HUB
 echo "Pull the latest image from DOCKER HUB"
 docker pull $IMAGE_NAME
-
-
-# docker run -d -p 8081:8080 905418199107.dkr.ecr.ap-northeast-2.amazonaws.com/spring-cd-test
-# sudo ./nginx -s reload
-# sudo ./nginx -t
-# sudo lsof -t -i :8081
-# sudo kill -15 45941
